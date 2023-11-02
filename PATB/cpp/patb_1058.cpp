f70dbb8c332eeb57e42b11d660f90afd417d556f
yaojun <940334249@qq.com>
2023-11-02 09:49:04 +0800

    add pat basic 1058

--- PATB/cpp/patb_1058.cpp
+++ PATB/cpp/patb_1058.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 struct pro {
     int f, fn, rn;  // 满分，总选项个数，正确选项个数
-    char str[6];    // 正确选项内容
+    char str[6];    // 正确选项内容 ac  abc
     int wrong, no;  // 错的人数 ,编号
 };
 int hash[110] = {0};
@@ -22,12 +22,13 @@ int main() {
     vector<pro> v;
     v.clear();
     scanf("%d%d", &N, &M);
+    // M道题
     for (int i = 0; i < M; i++) {
         scanf("%d%d%d", &f, &fn, &rn);
         temp.f = f;
         temp.fn = fn;
         temp.rn = rn;
-        temp.no = i + 1;
+        temp.no = i + 1;  // 编号从1开始
         int j;
         for (j = 0; j < rn; j++) {
             scanf(" %c", &c);
@@ -36,11 +37,13 @@ int main() {
         temp.str[j] = '\0';
         v.push_back(temp);
     }
-    int x, single, maxw = 0;
+    int x, single, max_wrong = 0;
     char s[6];
     for (int i = 0; i < N; i++) {
         single = 0;
+        // 有M道题目
         for (int j = 0; j < M; j++) {
+            // 本道题选了x个选项
             scanf(" (%d", &x);
             int k;
             for (k = 0; k < x; k++) {
@@ -48,23 +51,27 @@ int main() {
                 s[k] = c;
             }
             s[k] = '\0';
+            // 直接比较字符串 题目保证了选项是按顺序给的，即abcd
             if (strcmp(v[j].str, s) == 0) {
                 single += v[j].f;
             } else {
+                // 记录做错的数量
                 v[j].wrong++;
-                if (v[j].wrong > maxw) maxw = v[j].wrong;
+                if (v[j].wrong > max_wrong) max_wrong = v[j].wrong;
             }
             scanf("%c", &c);
         }
         printf("%d\n", single);
     }
-    if (maxw == 0) {
+    // 没有人做错
+    if (max_wrong == 0) {
         printf("Too simple\n");
         return 0;
     }
     sort(v.begin(), v.end(), cmp);
+    // 输出错得最多的次数和编号
     for (int i = 0; i < v.size(); i++) {
-        if (v[i].wrong == maxw) {
+        if (v[i].wrong == max_wrong) {
             if (i == 0) printf("%d", v[i].wrong);
             printf(" %d", v[i].no);
         } else
